ab0ee6cd9bddf140af3cdd9e762c1d9bc2055c42
Gonzalo Roget <gonxo89@gmail.com>
2019-10-12 12:44:55 -0300

    Agregar archivos de proyecto.

---
+++ PrimeraAplicacion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos a C#"); //Esto imprime un mensaje en consola
+        }
+    }
+}
